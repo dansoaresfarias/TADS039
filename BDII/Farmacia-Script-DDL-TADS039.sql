ad4e14ce19481ba914c1bceda4e204a9b692ebd3
Danilo Farias <danilo.silva@nutes.ufpe.br>
2025-03-24 12:12:26 -0300

    Aula de SQL

--- BDII/Farmacia-Script-DDL-TADS039.sql
+++ BDII/Farmacia-Script-DDL-TADS039.sql
@@ -1,4 +1,47 @@
 -- SQL: DDL - CREATE
+create schema trabalhador;
 
+use trabalhador;
+
+CREATE TABLE Cliente (
+    cpf VARCHAR(14) PRIMARY KEY NOT NULL,
+    nome VARCHAR(45) NOT NULL,
+    genero CHAR(1) NOT NULL,
+    email VARCHAR(45) UNIQUE NULL,
+    telefone VARCHAR(15) UNIQUE NOT NULL,
+    dataNasc DATE NULL,
+    `status` BOOLEAN NULL
+);
+
+CREATE TABLE Plano_Saude (
+    Cliente_cpf VARCHAR(14) PRIMARY KEY NOT NULL,
+    numero VARCHAR(20) UNIQUE NOT NULL,
+    nome VARCHAR(25) NOT NULL,
+    FOREIGN KEY (Cliente_cpf)
+        REFERENCES cliente (cpf)
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
 
 -- SQL: DDL - ALTER
+alter table departamento
+	add column descricao varchar(150) null;
+
+desc departamento;
+
+alter table departamento
+	change column descricao sobre varchar(155) not null;
+
+alter table departamento
+	drop column sobre;
+
+alter table cliente
+	add column potuacao int null default 0;
+    
+alter table cliente
+	add column idade int not null after dataNasc,
+    change column potuacao pontuacao int null default 0;
+
+desc cliente;
+
+alter table cliente
+	drop column idade;
---
+++ BDII/Farmacia-Script-DML-Insert-TADS039.sql
@@ -0,0 +1,6 @@
+-- SQL: DML - INSERT
+
+insert into cliente (cpf, nome, sexo, email, telefone, dataNasc)
+	value ("715.571.511-77", "Josué Heitor Oliveira", 'M', 
+			"jho@gmail.com", "(81)99508-0895", '2005-10-31');
+            
